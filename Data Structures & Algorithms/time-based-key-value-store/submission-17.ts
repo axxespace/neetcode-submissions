0edaf769d9924017c72d03abeef361595b2db28d
Nika Beridze <70541133+axxespace@users.noreply.github.com>
2026-08-11 06:15:03 +0400

    Add: time-based-key-value-store - submission-17

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-17.ts
@@ -0,0 +1,52 @@
+class TimeMap {
+    keyStore: Map<string, {value: string, timestamp: number}[]>
+
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key: string, value: string, timestamp: number): void {
+        const list = this.keyStore.get(key) || [];
+        this.keyStore.set(key, [...list, { value, timestamp }]);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key: string, timestamp: number): string {
+        if(!this.keyStore.has(key)) return '';
+        const arr = this.keyStore.get(key);
+        let left = 0;
+        let right = arr.length - 1;
+        while(left <= right) {
+            const mid = left + Math.floor((right - left) / 2);
+            if(arr[mid].timestamp === timestamp){
+                return arr[mid].value;
+            }
+            if(left === right && arr[mid].timestamp < timestamp) {
+                return arr[mid].value;
+            }
+            if(left === right && arr[mid].timestamp > timestamp) {
+                if(mid > 0) {
+                    return arr[mid - 1].value
+                }
+                else return ''
+            
+            }
+            if(arr[mid].timestamp < timestamp) {
+                left = mid + 1;
+            }
+            if(arr[mid].timestamp > timestamp) {
+                right = mid;
+            }
+        }
+    }
+}
