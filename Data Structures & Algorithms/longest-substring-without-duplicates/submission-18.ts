ab50edd95a7da22b9f722552b72e62b493b256ce
Nika Beridze <70541133+axxespace@users.noreply.github.com>
2026-08-14 10:48:26 +0400

    Add: longest-substring-without-duplicates - submission-18

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-18.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        let sum = 0;
+        let left = 0;
+        let right = s.length > 1 ? 1 : 0;
+        while (right < s.length) {
+            const slicedStr = s.slice(left, right);
+            if(!slicedStr.includes(s[right])) {
+                sum = right - left + 1 > sum ? right - left + 1 : sum;
+            }
+            else {
+                sum = sum < 1 ? 1 : sum;
+                left = left + slicedStr.indexOf(s[right]) + 1;
+            }
+            right ++
+        }
+        return sum;
+    }
+}
