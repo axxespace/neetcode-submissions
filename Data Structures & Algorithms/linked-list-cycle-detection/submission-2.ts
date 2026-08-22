a746ba738ad7bf2f9ac52c4bd47ee61184533c56
Nika Beridze <70541133+axxespace@users.noreply.github.com>
2026-08-22 22:48:47 +0400

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head: ListNode | null): boolean {
+        let current = head;
+        const arr = []
+        while (current !== null) {
+            if (arr.includes(current)) {
+                return true
+            }
+            arr.push(current);
+            current = current.next;
+        }
+        return false;
+    }
+}
