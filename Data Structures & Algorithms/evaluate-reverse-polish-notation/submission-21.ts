48cb7fd693b4db5ad30dbcb35c0c5dac3872c7c5
Nika Beridze <70541133+axxespace@users.noreply.github.com>
2026-08-25 17:35:07 +0400

    Add: evaluate-reverse-polish-notation - submission-21

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-21.ts
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens: string[]): number {
+        const arr: number[] = [];
+        for (let i = 0; i < tokens.length; i++) {
+            if(tokens[i] === '-' 
+            || tokens[i] === '+' 
+            || tokens[i] === '*' 
+            || tokens[i] === '/'){
+                const right = arr.pop();
+                const left = arr.pop();
+
+                switch(tokens[i]) {
+                    case "+":
+                        arr.push(Math.trunc(left + right));
+                    break;
+                    case "-":
+                        arr.push(Math.trunc(left - right));
+                    break;
+                    case "*":
+                        arr.push(Math.trunc(left * right));
+                    break;
+                    case "/":
+                        arr.push(Math.trunc(left / right));
+                    break;
+                }
+            }
+            else {
+                arr.push(parseInt(tokens[i]))
+            }
+        }
+
+        return arr[0];
+    }
+}
