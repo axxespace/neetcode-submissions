b97f104b9693d62094f2234b22da2e4110aa9aa6
Nika Beridze <70541133+axxespace@users.noreply.github.com>
2026-08-26 00:06:26 +0400

    Add: lru-cache - submission-10

---
+++ Data Structures & Algorithms/lru-cache/submission-10.ts
@@ -0,0 +1,42 @@
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+
+    capacity: number;
+    map: Map<number, number>;
+
+    constructor(capacity: number) {
+        this.capacity = capacity;
+        this.map = new Map();
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key: number): number {
+        if(this.map.has(key)) {
+            const val = this.map.get(key);
+            this.map.delete(key);
+            this.map.set(key, val);
+            return val;
+        }
+        return -1;
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key: number, value: number): void {
+        if (this.map.has(key)) {
+            this.map.delete(key);
+        } 
+        if(this.map.size >= this.capacity) {
+            this.map.delete(this.map.keys().next().value);
+        }
+        this.map.set(key, value);
+    }
+}
