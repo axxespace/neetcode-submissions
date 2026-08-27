eafd7624ca450db4c8c70a3d443b8865d963f3dc
Nika Beridze <70541133+axxespace@users.noreply.github.com>
2026-08-27 20:27:59 +0400

    Add: reverse-nodes-in-k-group - submission-2

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-2.ts
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} k
+     * @return {ListNode}
+     */
+    reverseKGroup(head: ListNode | null, k: number): ListNode | null {
+        if (!head || k === 1) return head;
+
+        const dummy = new ListNode(0, head);
+        let groupPrev = dummy;
+
+        while (true) {
+            const kth = this.getKth(groupPrev, k);
+            if (!kth) break;
+            const groupNext = kth.next;
+
+            let prev = kth.next;
+            let curr = groupPrev.next;
+            while (curr !== groupNext) {
+                const temp = curr!.next;
+                curr!.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            const temp = groupPrev.next;
+            groupPrev.next = kth;
+            groupPrev = temp!;
+        }
+
+        return dummy.next;
+    }
+
+    private getKth(curr: ListNode | null, k: number): ListNode | null {
+        while (curr && k > 0) {
+            curr = curr.next;
+            k--;
+        }
+        return curr;
+    }
+}
